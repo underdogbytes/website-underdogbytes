fdd6a2e071dff5b1519cdd201e45f453e98f20b1
bea <beatriztattooer@gmail.com>
2025-03-06 21:01:32 -0300

    :construction: new-component: footer

---
+++ resources/views/components/utils/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="inner">
+        <small>
+            Feito com Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </small>
+    </div>
+</footer>
