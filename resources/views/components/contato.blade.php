9965686e856ca9f3bfb9377b68df93370dd55c74
bea <beatriztattooer@gmail.com>
2025-03-25 22:44:45 -0300

    :sparkles: home[contato]: nova seção

---
+++ resources/views/components/contato.blade.php
@@ -0,0 +1,9 @@
+<section id="contato" class="contato">
+  <div class="inner">
+    <h2>Contato</h2>
+    <ul>
+      <li><a href="https://www.linkedin.com/in/btrz/">💼 https://www.linkedin.com/in/btrz/</a></li>
+      <li><a href="mailto:underdogbytes@gmail.com.br">📧 underdogbytes@gmail.com.br</a></li>
+    </ul>
+  </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
                 Meu nome é Beatriz Lima, engenheira de software e criadora de bugigangas.
                 <br>
                 Explore alguns dos meus projetos abaixo e
-                entre em contato para saber mais sobre meu trabalho
+                entre em <a href="#contato">contato</a> para saber mais sobre meu trabalho
                 em desenvolvimento web,
                 design de interfaces, ilustrações e soluções personalizadas.
                 <br><br>
@@ -26,5 +26,6 @@
     </div>
 
     <x-projetos.galeria />
+    <x-contato />
 </section>
 @endsection
