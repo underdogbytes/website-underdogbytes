7c86857b519f185f25d98329403ce05ef7af1984
bea <beatriztattooer@gmail.com>
2025-03-25 23:00:51 -0300

    :recycle: refactor[home]: reorganização de estrutura

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,24 @@
+<section>
+  <x-utils.titulo titulo="Bem-vindo ao meu website! :D" />
+
+  <div class="inner">
+    <div class="hero__content">
+      <p>
+        Bom dia! ^-^
+        <br>
+        Espero que esteja bem.
+      </p>
+
+      <p>
+        Meu nome é Beatriz Lima, engenheira de software e criadora de bugigangas.
+        <br>
+        Explore alguns dos meus projetos abaixo e
+        entre em <a href="#contato">contato</a> para saber mais sobre meu trabalho
+        em desenvolvimento web,
+        design de interfaces, ilustrações e soluções personalizadas.
+        <br><br>
+        Até breve! ♥
+      </p>
+    </div>
+  </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section>
-    <x-utils.titulo titulo="Bem-vindo ao meu website! :D" />
-
-    <div class="inner">
-        <div class="hero__content">
-            <p>
-                Bom dia! ^-^
-                <br>
-                Espero que esteja bem.
-            </p>
-            
-            <p>
-                Meu nome é Beatriz Lima, engenheira de software e criadora de bugigangas.
-                <br>
-                Explore alguns dos meus projetos abaixo e
-                entre em <a href="#contato">contato</a> para saber mais sobre meu trabalho
-                em desenvolvimento web,
-                design de interfaces, ilustrações e soluções personalizadas.
-                <br><br>
-                Até breve! ♥
-            </p>
-        </div>
-    </div>
-
+    <x-hero />
     <x-projetos.galeria />
     <x-contato />
-</section>
 @endsection
