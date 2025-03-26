24ce9161287ae60a98736da3937ca24d41e36147
bea <beatriztattooer@gmail.com>
2025-03-25 22:24:23 -0300

    :mag: home[hero]: melhoria de SEO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,24 @@
     <x-utils.titulo titulo="Bem-vindo ao meu website! :D" />
 
     <div class="inner">
-        <p>
-            Bom dia! ^-^
-            <br>
-            Espero que esteja bem.
-        </p>
-
-        <p>
-            Meu nome é Beatriz Lima, engenheira de Software e criadora de bugigangas.
-            <br>
-            Veja alguns dos meus projetos abaixo e até breve! ♥
-        </p>
+        <div class="hero__content">
+            <p>
+                Bom dia! ^-^
+                <br>
+                Espero que esteja bem.
+            </p>
+            
+            <p>
+                Meu nome é Beatriz Lima, engenheira de software e criadora de bugigangas.
+                <br>
+                Explore alguns dos meus projetos abaixo e
+                entre em contato para saber mais sobre meu trabalho
+                em desenvolvimento web,
+                design de interfaces, ilustrações e soluções personalizadas.
+                <br><br>
+                Até breve! ♥
+            </p>
+        </div>
     </div>
 
     <x-projetos.galeria />
