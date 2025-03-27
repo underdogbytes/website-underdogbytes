e214889739f27285961eaf8dead1a8f4d233a203
bea <beatriztattooer@gmail.com>
2025-03-26 23:30:59 -0300

    :sparkles: feat[componente]: card y2k

---
+++ resources/views/components/utils/cards/y2k.blade.php
@@ -0,0 +1,13 @@
+<div class="card card--y2k">
+  <div class="card__header">
+    <h4 class="card__header__title">{!! $titulo !!}</h4>
+
+    <div class="card__header__buttons-container">
+      <button>X</button>
+    </div>
+  </div>
+
+  <div class="card__body">
+    {!! $texto !!}
+  </div>
+</div>
