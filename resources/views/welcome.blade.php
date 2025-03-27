9d109e013507c498508885f52e43a62b439d7977
bea <beatriztattooer@gmail.com>
2025-03-26 23:33:32 -0300

    :sparkles: feat[home]: novo layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-hero />
+    <x-utils.titulo titulo="Bem-vindo ao meu website! :D" />
+    <div class="inner">
+        <x-utils.cards.y2k
+            titulo="Você recebeu uma mensagem"
+            texto="<p>
+            Bom dia! ^-^
+            <br>
+            Espero que esteja bem.
+            </p>
+
+            <p>
+                Meu nome é Beatriz Lima, engenheira de software e criadora de bugigangas.
+                <br>
+                Explore alguns dos meus projetos abaixo e
+                entre em <a href='#contato'>contato</a> para saber mais sobre meu trabalho
+                em desenvolvimento web,
+                design de interfaces, ilustrações e soluções personalizadas.
+                <br><br>
+                Até breve! ♥
+            </p>"
+        />
+   </div>
     <x-projetos.galeria />
     <x-contato />
 @endsection
