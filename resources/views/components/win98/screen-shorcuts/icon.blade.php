39d47d695e5eca46c9f3cceba50b07b1e6d838e3
bea <beatriztattooer@gmail.com>
2025-04-01 17:07:41 -0300

    :recycle: :sparkles: feat-refactor[home]: icones

--- resources/views/components/win98/screen-shorcuts/icon.blade.php
+++ resources/views/components/win98/screen-shorcuts/icon.blade.php
@@ -1,6 +1,6 @@
-<a href="{{ asset('documentos/cv-pt-2025.pdf') }}" download>
-  <div class="win98__screen-icon__container">
-    <img src="{{ asset('imagens/icones/pasta.png') }}" alt="ícone de pasta com um papel" />
-    <span>Meu Currículo</span>
+<a href="{{ asset($link) }}" $tipo>
+  <div class="win98__screen-icon__container__list-icons__icon">
+    <img src="{{ asset($imagemSrc) }}" alt="{{ $imagemAlt }}" />
+    <span>{{ $nome }}</span>
   </div>
 </a>
--- resources/views/components/win98/screen-shorcuts/index.blade.php
+++ resources/views/components/win98/screen-shorcuts/index.blade.php
@@ -1,5 +1,20 @@
 <section>
   <div class="inner">
-    <x-win98.screen-shorcuts.icon />
+    <div class="win98__screen-icon__container">
+      <x-win98.screen-shorcuts.icon
+        nome="Meu Currículo"
+        imagemAlt="ícone de pasta com um papel"
+        imagemSrc="imagens/icones/pasta.png"
+        link="documentos/cv-pt-2025.pdf"
+        tipo="download"
+      />
+
+      <x-win98.screen-shorcuts.icon
+        nome="Perguntas Frequentes"
+        imagemAlt="ícone de pasta com um papel"
+        imagemSrc="imagens/icones/ajuda.png"
+        link="#perguntas-frequentes"
+      />
+    </div>
   </div>
 </section>
