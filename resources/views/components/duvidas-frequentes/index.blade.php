4dcb132b5e4c8cdcd221d23c6414dcf8d0aad740
bea <beatriztattooer@gmail.com>
2025-04-01 17:06:38 -0300

    :recycle: wip[refactor]: dúvidas frequentes

---
+++ resources/views/components/duvidas-frequentes/index.blade.php
@@ -0,0 +1,10 @@
+<section id="perguntas-frequentes">
+  <div class="inner">
+    <div class="contato__header">
+      <x-utils.titulo titulo="Perguntas frequentes" />
+      <img src="{{ asset('imagens/icones/clippy.webp') }}" alt="ícone de clipe do microsoft word" />
+    </div>
+
+    <x-duvidas-frequentes.duvida />
+  </div>
+</section>
