8b075bc53d6e535965b0906cdbaf33ffa6497a42
bea <beatriztattooer@gmail.com>
2025-09-26 17:11:33 -0300

    :construction: wip: atualização bio page sobre-mim

---
+++ resources/views/components/sobre/bio.blade.php
@@ -0,0 +1,33 @@
+<div>
+  <p>
+    Olá, meu nome é Beatriz Pereira Lima :)
+    <br>
+    Sou a pessoa da foto aqui:
+  </p>
+
+  <img src="{{ asset('imagens/eu.webp') }}" alt="Pessoa de cabelo médio olhando para baixo
+      segurando um teclado mecânico com uma mão
+      e usando um negócio de remover tecla em outra mão" width="250" />
+
+  <p>
+    Sou um animal simples: gosto de plantas, animais, comer e estudar.
+    Acredito que o sentido da vida está em aproveitar a jornada vivendo o momento presente e
+    ter um bom balanço entre o descans e o se manter em movimento.
+  </p>
+
+  <p>
+    Para fins trabalhistas:
+    <br>
+    Quero deixar evidente que a decisão de fazer um website
+    como nos primórdios da internet foi uma decisão lúcida.
+    <br>
+    Genuinamente estou cansada de codar tanta coisa comercial.
+    E essa escolha além de aliviar minha alma,
+    me permite ter mais tempo para criar minhas coisas,
+    focando no conteúdo e menos na aaaaaesthethic :p
+    <br><br>
+    Na verdade, podem falar muitas coisas do meu website.
+    <br>
+    Menos que ele não funciona e carrega rápido. :D
+  </p>
+</div>
