41ac917261551feb5e2db523035ebb5dfbf701d5
bea <beatriztattooer@gmail.com>
2025-10-03 15:36:21 -0300

    :pencil: update: blog

--- resources/views/paginas/blog.blade.php
+++ resources/views/paginas/blog.blade.php
@@ -7,7 +7,28 @@
       <x-utils.titulo titulo="Blog da Bea" />
       <div>
         <p>
-          às vezes eu escrevo, certamente hoje não é um desses dias :D
+          Uma vez eu escrevi meio livro no fundamental.
+          E digo meio livro, porque nunca voltei para terminar a história.
+          E teve uma vez em fiquei em primeiro lugar com uma redação no ensino médio.
+          Então um dia coloquei fogo (literalmente) em quase tudo que produzi, de desenhos e textos.
+        </p>
+        <p>
+          Acontece.
+        </p>
+        <p>
+          Ainda escrevo.
+        </p>
+        <p>Às vezes.</p>
+        <br><br>
+        <p>
+          Para ler meus textos e também me conhecer mais,
+          <a
+            href="https://bridgetocross.neocities.org"
+            target="_blank"
+          >
+            clique aqui
+          </a>
+          .
         </p>
       </div>
     </div>
