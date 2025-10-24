def3fe0921c1c7975499795d74ff2a54d17613aa
bea <beatriztattooer@gmail.com>
2025-10-24 16:07:37 -0300

    :sparkles: taskbar

--- resources/views/components/utils/footer.blade.php
+++ resources/views/components/utils/footer.blade.php
@@ -1,30 +1,28 @@
 <footer>
-    <div class="inner">
-        <div class="btns">
-            <x-utils.buttons.default
-                link="/"
-                label="Home"
-            />
-            <x-utils.buttons.default
-                link="aleatoriedades"
-                label="Aleatoriedades"
-            />
-            <x-utils.buttons.default
-                link="blog"
-                label="Blog"
-            />
-            <x-utils.buttons.default
-                link="sobre"
-                label="Sobre mim"
-            />
+    <div id="taskMenu" class="taskbar__menu">
+        <div class="taskbar__menu__content">
+            <x-utils.buttons.default link="/" label="Home" />
+            <x-utils.buttons.default link="aleatoriedades" label="Aleatoriedades" />
+            <x-utils.buttons.default link="blog" label="Blog" />
+            <x-utils.buttons.default link="sobre" label="Sobre mim" />
             <x-utils.buttons.back-to-top />
+
+            <small class="credits">
+                ˙✧˖° Programado em Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                a base de muito yakissoba, chá e carinho de gatos ⋆｡˚✧
+                <br>
+                © by <a href="{{ url('/sobre') }}">Underdogbytes</a> 2025
+            </small>
         </div>
-        <br>
-        <small>
-            ˙✧˖° Programado em Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-            a base de muito yakissoba, chá e carinho de gatos ⋆｡˚✧
-            <br>
-            © by <a href="{{ url('/sobre') }}">Underdogbytes</a> 2025
-        </small>
+    </div>
+
+    <div class="taskbar">
+        <button
+            id="btn_start"
+            class="start"
+        >
+            Início
+        </button>
+        <div class="tray">23:59</div>
     </div>
 </footer>
