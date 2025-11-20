a65dad240dc885b1b27d945058af8f9d6ca02a50
bea <beatriztattooer@gmail.com>
2025-11-20 14:11:08 -0300

    manutenção

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,30 +2,5 @@
 @section('title', 'Página Inicial')
 
 @section('content')
-    <div class="inner">
-        <x-win98.screen-shorcuts />
-        {{-- <x-utils.cards.y2k
-            tipo="h3"
-            titulo="Você recebeu uma mensagem"
-            texto="<p>
-            Bom dia! ^-^
-            <br>
-            Espero que esteja bem.
-            </p>
-
-            <p>
-                Meu nome é Beatriz Lima, engenheira de software e criadora de bugigangas.
-                <br>
-                Explore alguns dos meus projetos abaixo e
-                entre em <a href='#contato'>contato</a> para saber mais sobre meu trabalho
-                em desenvolvimento web,
-                design de interfaces, ilustrações e soluções personalizadas.
-                <br><br>
-                Até breve! ♥
-            </p>"
-        /> --}}
-   </div>
-    <x-projetos.destaques-home />
-    <x-duvidas-frequentes />
-    <x-contato />
+<p>Em manutenção, volte em breve :)</p>
 @endsection
