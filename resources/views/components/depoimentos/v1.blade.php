83e8709b347c2bbc4b785e84fa04701eadbe4024
bea <beatriztattooer@gmail.com>
2025-11-28 00:04:27 -0300

    :pencil: texto: depoimentos

--- resources/views/components/depoimentos/v1.blade.php
+++ resources/views/components/depoimentos/v1.blade.php
@@ -1,7 +1,7 @@
 <section>
   <div class="section-title container-max-width">
     <h2>Depoimentos</h2>
-    <div class="section-sub">O que clientes falam</div>
+    <div class="section-sub">O que nossos clientes falam</div>
   </div>
 
   <div class="wrap">
@@ -13,8 +13,6 @@
         clientName="Chico Luís, artista"
         service="Website Portfólio"
       />
-      <div class="item">“Entrega no prazo e design com personalidade.” — Cliente B</div>
-      <div class="item">“Código organizado e comunicação clara.” — Cliente C</div>
     </div>
   </div>
 </section>
