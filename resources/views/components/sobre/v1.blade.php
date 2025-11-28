3853aaf3d2a027a0f22dd8d345c63c03f9224812
bea <beatriztattooer@gmail.com>
2025-11-27 23:45:09 -0300

    :lipstick: ajusteseção sobre

--- resources/views/components/sobre/v1.blade.php
+++ resources/views/components/sobre/v1.blade.php
@@ -1,16 +1,16 @@
 <section id="sobre" class="wrap about">
   <div class="photo pixel-border">
-    <img src="/images/bea-photo.jpg" alt="Foto Bea" style="width:100%;height:100%;object-fit:cover;min-height:260px">
+    <x-logo.cat />
   </div>
   <div class="bio">
-    <h3>Olá, eu sou a Bea</h3>
-    <p>Sou desenvolvedora front-end e designer UI. Gosto de misturar estética retrô com interfaces modernas, pensando
-      sempre em performance, acessibilidade e usabilidade.</p>
+    <h3>Olá, somos a Underdog Bytes</h3>
+    <p>Desenvolvemos soluções digitais baseadas em performance, acessibilidade e usabilidade.</p>
 
     <div class="skills">
-      <span class="chip">HTML & CSS</span>
-      <span class="chip">JavaScript</span>
-      <span class="chip">Vue / React</span>
+      <span class="chip">HTML & CSS / Bootstrap</span>
+      <span class="chip">JavaScript / Vue</span>
+      <span class="chip">PHP / Laravel</span>
+      <span class="chip">WordPress / OpenCart</span>
       <span class="chip">Design System</span>
     </div>
   </div>
