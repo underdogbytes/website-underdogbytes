43321818b8848287f286056c6af0fc74beddf348
bea <beatriztattooer@gmail.com>
2025-11-28 00:04:43 -0300

    :pencil: texto: projetos

--- resources/views/components/projetos/minimalist/v1.blade.php
+++ resources/views/components/projetos/minimalist/v1.blade.php
@@ -6,17 +6,17 @@
   <div class="wrap">
     <div class="projects-grid">
       <x-projetos.minimalist.card
-        id="p2"
+        id="p1"
         name="EBD Global Optimum"
-        description="Website WordPress"
+        description="Website WordPress para agência de consultoria ambiental"
         imgSrc="/images/projects/ebdglo/thumb.png"
         imgAlt="Floresta ao fundo com a logo branca na frente"
       />
 
       <x-projetos.minimalist.card
-        id="p1"
+        id="p2"
         name="Coleções Fauna do Brasil"
-        description="E-commerce OpenCart"
+        description="E-commerce OpenCart para artesão de pelúcias em Curitiba"
         imgSrc="/images/projects/faunabr/thumb.png"
         imgAlt="Pelúcia de Raia Manta da Coleção Fauna do Brasil"
       />
@@ -24,9 +24,57 @@
       <x-projetos.minimalist.card
         id="p3"
         name="Mais Futuro"
-        description="Sistema de Previdência Privada"
+        description="Sistema de Previdência Privada Desktop e Mobile"
         imgSrc="/images/projects/mais-futuro/thumb.png"
         imgAlt="Mockup de celular"
+      />
+
+      <x-projetos.minimalist.card
+        id="p4"
+        name="Escola Renovação"
+        description="Website para escola"
+        imgSrc="/images/projects/escola-renovacao/thumb.png"
+        imgAlt="Mulher sorrindo com fone e mala ao lado de uma águia com o fundo laranja"
+      />
+
+      <x-projetos.minimalist.card
+        id="p5"
+        name="Tempestade Criativa"
+        description="Website para agência de marketing"
+        imgSrc="/images/projects/tempestade-criativa/thumb.png"
+        imgAlt="Mulher sorrindo pulando com notebook em mãos com um raio rosa atrás e o fundo azul"
+      />
+
+      <x-projetos.minimalist.card
+        id="p6"
+        name="Show Gospel"
+        description="Website blog Laravel para revista gospel"
+        imgSrc="/images/projects/show-gospel/thumb.png"
+        imgAlt="Revista Show Gospel de 20 anos com capa preta e imagem da equipe de filmagem Signature"
+      />
+
+      <x-projetos.minimalist.card
+        id="p7"
+        name="CEJ"
+        description="Sistema para colégio"
+        imgSrc="/images/projects/cej/thumb.png"
+        imgAlt="Prestação de serviço para colégio"
+      />
+
+      <x-projetos.minimalist.card
+        id="p8"
+        name="ABCD Edu Edu"
+        description="Sistema para dislexia"
+        imgSrc="/images/projects/abcd/thumb.png"
+        imgAlt="Prestação de serviço para aplicativo desktop e mobile que ajuda crianças com dislexia"
+      />
+
+      <x-projetos.minimalist.card
+        id="p9"
+        name="Sorla DB"
+        description="UI/UX de Documetação"
+        imgSrc="/images/projects/sorla-db/thumb.png"
+        imgAlt="Mockup para documentação de biblioteca open-source javaScript e vetorização de logo"
       />
     </div>
   </div>
